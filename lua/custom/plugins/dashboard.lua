9a9f4c96b2efc0b60ac3a889672c21312baff157
dieguscl <diegocastrolaya@gmail.com>
2024-12-09 21:14:15 -0500

    dashboard

--- lua/custom/plugins/dashboard.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'nvimdev/dashboard-nvim',
-  config = function()
-    require('dashboard').setup {
-      theme = 'hyper', --  theme is doom and hyper default is hyper
-    }
-  end,
-  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
-}
